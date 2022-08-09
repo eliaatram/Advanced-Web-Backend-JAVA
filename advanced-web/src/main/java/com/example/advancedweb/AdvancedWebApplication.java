a611f30e48e604e2100c277fb65913e86c37fd1a
Elia El Atram <72523490+eliaatram@users.noreply.github.com>
2022-08-09 13:59:40 +0200

    initialized new spring boot project

---
+++ advanced-web/src/main/java/com/example/advancedweb/AdvancedWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.advancedweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvancedWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdvancedWebApplication.class, args);
+    }
+
+}
---
+++ advanced-web/src/test/java/com/example/advancedweb/AdvancedWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.advancedweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvancedWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
