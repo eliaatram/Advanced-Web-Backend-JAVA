a178b29c6e5e660d34506b2c05ba06a1c3aa00c3
Elia El Atram <72523490+eliaatram@users.noreply.github.com>
2022-08-09 14:05:28 +0200

    Aded movie service interface

--- advanced-web/src/main/java/com/example/advancedweb/service/MovieService.java
+++ advanced-web/src/main/java/com/example/advancedweb/service/MovieService.java
@@ -1,2 +1,16 @@
-package com.example.advancedweb.service;public interface MovieService {
+package com.example.advancedweb.service;
+
+import com.example.advancedweb.datamodel.Movies;
+import com.example.advancedweb.payloads.request.MovieRequest;
+import com.example.advancedweb.payloads.response.MessageResponse;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public interface MovieService {
+    MessageResponse createMovie(MovieRequest movieRequest);
+    void deleteMovie(Integer movieId);
+    Movies getASingleMovie(Integer movieId);
+    List<Movies> getAllMovies();
 }
