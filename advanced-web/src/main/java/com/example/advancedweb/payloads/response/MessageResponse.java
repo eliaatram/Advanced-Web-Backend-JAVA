e771c76d87feca00b8c93c47fb92ba9452b603b9
Elia El Atram <72523490+eliaatram@users.noreply.github.com>
2022-08-09 14:05:14 +0200

    added message response class

--- advanced-web/src/main/java/com/example/advancedweb/payloads/response/MessageResponse.java
+++ advanced-web/src/main/java/com/example/advancedweb/payloads/response/MessageResponse.java
@@ -1,2 +1,18 @@
-package com.example.advancedweb.payloads.response;public class MessageResponse {
+package com.example.advancedweb.payloads.response;
+
+public class MessageResponse {
+
+    private String message;
+
+    public MessageResponse(String message){
+        this.message = message;
+    }
+
+    public void setMessage(String message){
+        this.message = message;
+    }
+
+    public String getMessage(){
+        return message;
+    }
 }
